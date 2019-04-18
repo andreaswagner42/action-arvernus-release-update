6327a2f83bfb34fd7e11ae3517ad2c04f701df31
fabiankaegy <fabian@arvernus.info>
2019-04-18 07:39:10 +0200

    working event data

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,11 +46,11 @@ rsync -r \
 VERSION=${GITHUB_REF#refs/tags/}
 
 
-LATEST_RELEASE_NAME=($(jq -r '.release.name' /github/workflow/event.json))
+LATEST_RELEASE_NAME=$(jq -r '.release.name' /github/workflow/event.json)
 echo "$LATEST_RELEASE_NAME"
 
-LATEST_RELEASE_DESCRIPTION=($(jq -r '.release.body' /github/workflow/event.json))
-echo :`$LATEST_RELEASE_DESCRIPTION`
+LATEST_RELEASE_DESCRIPTION=$(jq -r '.release.body' /github/workflow/event.json)
+echo "$LATEST_RELEASE_DESCRIPTION"
 
 # change directory into the workspace 
 cd "$GITHUB_WORKSPACE"/
