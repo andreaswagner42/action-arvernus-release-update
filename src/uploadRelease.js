26f30c8f3a55e53d01b80e81d8ea870df08e405e
Fabian Kägy <fabian@arvernus.info>
2019-09-11 23:21:30 +0200

    logging

--- src/uploadRelease.js
+++ src/uploadRelease.js
@@ -9,6 +9,8 @@ async function uploadRelease(name, release, hostname, secret) {
 
 		const file = fs.createReadStream(release.file);
 
+		console.log("File", file);
+
 		const form = new FormData();
 		form.append("release_title", release.name);
 		form.append("release_notes", release.body);
@@ -19,13 +21,11 @@ async function uploadRelease(name, release, hostname, secret) {
 		const response = await fetch(url, { method: "POST", body: form });
 
 		if (!response.ok) {
-			console.log("response not ok");
 			throw await response.json();
 		}
 
 		const responseJson = await response.json();
 
-		console.log(responseJson);
 		return Promise.resolve(responseJson);
 	} catch (error) {
 		return Promise.reject(error);
