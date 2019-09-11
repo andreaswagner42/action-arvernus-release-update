9132bf06841ad7e96153af21805760c3499846fc
Fabian Kägy <fabian@arvernus.info>
2019-09-11 19:57:18 +0200

    hello world

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const action = async () => {
-	try {
-		const githubToken = core.getInput("github-access-token");
-		const octokit = new github.GitHub(githubToken);
-		core.debug("context: " + github.context.ref);
+try {
+	// const githubToken = core.getInput("github-access-token");
+	// const octokit = new github.GitHub(githubToken);
+	// core.debug("context: " + github.context.ref);
 
-		const ref = octokit.git.getRef({
-			...github.context.repo,
-			...github.context
-		});
+	// const ref = octokit.git.getRef({
+	// 	...github.context.repo,
+	// 	...github.context
+	// });
 
-		core.debug("ref: " + ref);
+	// core.debug("ref: " + ref);
 
-		const updateServerUrl = core.getInput("update-server-url");
-		core.debug("updateServerUrl: " + updateServerUrl);
-		const serverSecretKey = core.getInput("server-secret-key");
-		core.debug("serverSecretKey: " + serverSecretKey);
+	// const updateServerUrl = core.getInput("update-server-url");
+	// core.debug("updateServerUrl: " + updateServerUrl);
+	// const serverSecretKey = core.getInput("server-secret-key");
+	// core.debug("serverSecretKey: " + serverSecretKey);
 
-		const action = github.context.eventName;
-		const packageName = github.context.repo.repo;
-	} catch (error) {
-		core.warning(error);
-		core.setFailed(error.message);
-	}
-};
-
-action();
-
-module.exports = action;
+	// const action = github.context.eventName;
+	// const packageName = github.context.repo.repo;
+	core.debug("Hello World!");
+} catch (error) {
+	core.warning("Hello Catch!");
+	core.setFailed(error.message);
+}
