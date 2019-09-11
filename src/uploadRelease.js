8eecaa4558a43ca17829c81779db014e263dfe82
Fabian Kägy <fabian@arvernus.info>
2019-09-11 23:10:17 +0200

    file

--- src/uploadRelease.js
+++ src/uploadRelease.js
@@ -7,12 +7,14 @@ async function uploadRelease(name, release, hostname, secret) {
 	try {
 		const url = `${hostname}/package/${name}/${release.tag_name}?secret_key=${secret}`;
 
+		const file = fs.createReadStream(release.file);
+
 		const form = new FormData();
 		form.append("release_title", release.name);
 		form.append("release_notes", release.body);
 		form.append("prerelease", String(release.prerelease));
 		form.append("published_at", String(release.published_at));
-		form.append("file", release.file);
+		form.append("file", file);
 
 		const response = await fetch(url, { method: "POST", body: form });
 
